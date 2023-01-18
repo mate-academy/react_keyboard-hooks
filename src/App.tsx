531e8d5a40c7c95c399575668ef1da62df74f527
Ala El Achkar <iamalaaashkar97@gmail.com>
2023-01-18 23:31:14 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    {/* Keep this input for tests */}
-    <input type="text" />
+export const App: React.FC = () => {
+  const [key, setKey] = useState('');
+  const handleKey = (event: KeyboardEvent) => {
+    setKey(event.key);
+  };
 
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+  useEffect(() => {
+    document.addEventListener('keyup', handleKey);
+
+    return () => document.removeEventListener('keyup', handleKey);
+  }, []);
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+      />
+
+      {
+        key
+          ? <p className="App__message">{`The last pressed key is [${key}]`}</p>
+          : <p className="App__message">Nothing was pressed yet</p>
+      }
+    </div>
+  );
+};
