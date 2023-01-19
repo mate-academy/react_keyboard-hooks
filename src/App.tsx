9698b914a1d63499405cfbe37b8a3e32403a2173
Ilija Pejanovic <ikadvm96@gmail.com>
2023-01-19 12:58:54 +0100

    added input

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ export const App: React.FC = () => {
   return (
     <div className="App">
       {/* Keep this input for tests */}
+      <input type="text" />
       {
         usedKeyboardKey
           ? <p className="App__message">{`The last pressed key is [${usedKeyboardKey}]`}</p>
