2d0314852d05e7bb0a84e2229de800f86e6e6ee8
ctk12 <chetankatwa12@gmail.com>
2023-03-26 09:00:03 +0530

    Added React Keyboard with Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    {/* Keep this input for tests */}
-    <input type="text" />
+export const App: React.FC = () => {
+  const [pressedKey, setPressedKey] = useState('');
 
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+  useEffect(() => {
+    document.addEventListener('keyup', (event: KeyboardEvent) => {
+      setPressedKey(event.key);
+    });
+
+    return () => {
+      document.removeEventListener('keyup', (event: KeyboardEvent) => {
+        setPressedKey(event.key);
+      });
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      {/* Keep this input for tests */}
+      <input type="text" />
+
+      <p className="App__message">
+        {pressedKey === ''
+          ? 'Nothing was pressed yet'
+          : `The last pressed key is [${pressedKey}]`}
+      </p>
+    </div>
+  );
+};
